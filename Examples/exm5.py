0ef2ed2f51597c6567368e62de781e60eb5eccdc
CHANDAN PRAKASH <141991237+chandanprakash14@users.noreply.github.com>
2026-02-14 15:54:50 +0530

    Delete Examples directory

--- Examples/exm5.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-stu_performace=np.array(["Excellent","Good","Avg","Poor"])
-stu_values=np.array([15,25,12,8])
-plt.pie(stu_values,labels=stu_performace,autopct='%1.1f%%')
-plt.title("Students Performance")
-plt.show()
--- Examples/exmp3.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-a=np.array(['2016','2017','2018','2019','2020','2021','2022','2023','2024'])
-b=np.array([989,1092,649,1022,899,784,988,1100,879])
-plt.bar(a,b,color="hotpink")
-plt.xlabel("Years",color="blue",size=20)
-plt.ylabel("Runs",color="blue",size=20)
-plt.suptitle("Virat Kohli Test Cricket Runs",color="red",size=30)
-plt.show()
